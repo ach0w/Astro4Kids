0b5eb9c06df474c8e68edd53e8fe6a1a0e83f5de
ach0w <139292304+ach0w@users.noreply.github.com>
2023-08-19 11:54:32 -0400

    Update skybox.js

--- skybox.js
+++ skybox.js
@@ -1,1 +1,23 @@
+let scene, camera, renderer, skyboxGeo, skybox;
+function init() {
+scene = new THREE.Scene();
+camera = new THREE.PerspectiveCamera(
+    55,
+    window.innerWidth / window.innerHeight,
+    45,
+    30000
+);
+camera.position.set(1200, -250, 20000);
 
+renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.domElement.id = "canvas";
+document.body.appendChild(renderer.domElement);
+animate();
+}
+function animate() {
+renderer.render(scene, camera);
+requestAnimationFrame(animate);
+}
+
+init();
