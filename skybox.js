68f5353ef4eb94be4f83b1c0f54145d408ae4f21
ach0w <139292304+ach0w@users.noreply.github.com>
2023-08-21 17:48:15 -0400

    Update skybox.js

--- skybox.js
+++ skybox.js
@@ -1,30 +1,36 @@
 let scene, camera, renderer, skyboxGeo, skybox;
+
 function init() {
-scene = new THREE.Scene();
-camera = new THREE.PerspectiveCamera(
-    55,
-    window.innerWidth / window.innerHeight,
-    45,
-    30000
-);
-camera.position.set(1200, -250, 20000);
+    // Creates scene and camera
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(
+        55,
+        window.innerWidth / window.innerHeight,
+        45,
+        30000
+    );
+    camera.position.set(1200, -250, 20000);
+   
+    // This creates a canvas element and graphics become smoother with antialias
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.domElement.id = "canvas";
+    document.body.appendChild(renderer.domElement);
 
-renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.domElement.id = "canvas";
-document.body.appendChild(renderer.domElement);
-
+    // Creates the skybox
     skyboxGeo = new THREE.BoxGeometry(10000, 10000, 10000);
     skybox = new THREE.Mesh(skyboxGeo);
     scene.add(skybox);
-    
-animate();
+
+    // Animates the skybox
+    animate();
 }
+
 function animate() {
-  skybox.rotation.x += 0.005;
-  skybox.rotation.y += 0.005;
-  renderer.render(scene, camera);
-  requestAnimationFrame(animate);
+    skybox.rotation.x += 0.005;
+    skybox.rotation.y += 0.005;
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
 }
 
 init();
