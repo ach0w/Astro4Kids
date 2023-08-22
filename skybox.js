5403af817bfad08656a85e2e5386a0a32ae70588
ach0w <139292304+ach0w@users.noreply.github.com>
2023-08-21 20:02:45 -0400

    Update skybox.js

--- skybox.js
+++ skybox.js
@@ -36,8 +36,7 @@ function init() {
 }
 
 function animate() {
-    skybox.rotation.x += 0.005;
-    skybox.rotation.y += 0.005;
+    controls.update();
     renderer.render(scene, camera);
     requestAnimationFrame(animate);
 }
